f2aa0f921fca484b803c572d1f71e6aac3900a6e
jacob-blomq <jpb1029@wildcats.unh.edu>
2019-09-19 21:13:19 -0400

    implemented about page

--- pages/views.py
+++ pages/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
-
 from django.views.generic import TemplateView
-
 
 class HomePageView(TemplateView):
 
-	template_name = 'home.html'
+    template_name = 'home.html'
 
+class AboutPageView(TemplateView):
+	
+    template_name = 'about.html'
 
-from django.contrib import admin
-
-from django.urls import path, include #new
-
-
-urlpatterns = [
-
-	path('admin/', admin.site.urls),
-
-	path('', include('pages.urls')), # new
-
-]
-
-# Create your views here.
