0d0e3741e56bf5521821595af760423f66b2ff67
jacob-blomq <jpb1029@wildcats.unh.edu>
2019-09-19 20:53:49 -0400

    implemented homepage (succesfully this time)

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+
+	template_name = 'home.html'
+
+
+from django.contrib import admin
+
+from django.urls import path, include #new
+
+
+urlpatterns = [
+
+	path('admin/', admin.site.urls),
+
+	path('', include('pages.urls')), # new
+
+]
+
 # Create your views here.
